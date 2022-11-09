d66b5cdd43dc8a9a8286c8160a5d777124276eef
Robin Hossain <robinhossain5261@gmail.com>
2022-11-09 20:03:06 +0600

    products details complete

--- src/pages/Products/ProductDetails.js
+++ src/pages/Products/ProductDetails.js
@@ -1,8 +1,10 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { useLoaderData } from 'react-router-dom';
+import { AuthContext } from '../../contexts/AuthProvider/AuthProvider';
 
 const ProductDetails = () => {
     const { title, description, img, price, rating } = useLoaderData();
+    const { user } = useContext(AuthContext);
 
     return (
         <div >
@@ -13,6 +15,33 @@ const ProductDetails = () => {
                 <p>Price: {price}</p>
                 <p>Rating: {rating}</p>
                 <p><span className='font-bold'>Description :</span> {description}</p>
+            </div>
+
+            <div className='my-12'>
+                <h1 className='text-center text-4xl font-bold '>Please review this product</h1>
+                <form>
+                    <div>
+                        <div className="form-control w-full max-w-xs">
+                            <label className="label">
+                                <span className="label-text text-xl font-semibold">Your name</span>
+                            </label>
+                            <input type="text" placeholder="Type here" defaultValue={user?.displayName} className="input input-bordered w-full" />
+                        </div>
+                        <div className="form-control w-full max-w-xs">
+                            <label className="label">
+                                <span className="label-text text-xl font-semibold">Your email</span>
+                            </label>
+                            <input type="email" placeholder="Type here" defaultValue={user?.email} className="input input-bordered w-full" readOnly />
+                        </div>
+
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text text-xl font-semibold">Your comment</span>
+                            </label>
+                            <textarea className="textarea textarea-bordered h-24" placeholder="Your comment"></textarea>
+                        </div>
+                    </div>
+                </form>
             </div>
         </div>
     );
